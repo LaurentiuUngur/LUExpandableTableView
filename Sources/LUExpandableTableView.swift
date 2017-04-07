4fb77c65f62eb7b02196f8b2bfd9819573d1946e
Laurentiu Ungur <laurentyu1995@gmail.com>
2017-04-07 12:01:54 +0300

    Add more documentation

--- Sources/LUExpandableTableView.swift
+++ Sources/LUExpandableTableView.swift
@@ -108,6 +108,12 @@ open class LUExpandableTableView: UITableView {
 // MARK: - UITableViewDelegate
 
 extension LUExpandableTableView: UITableViewDelegate {
+    /** Asks the delegate for a view object to display in the header of the specified section of the table view.
+    
+    - Parameters:
+        - tableView: The table-view object asking for the view object.
+        - section: An index number identifying a section of `tableView`.
+    */
     public func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         guard let sectionHeader = expandableTableViewDataSource?.expandableTableView(self, sectionHeaderOfSection: section) else {
             return nil
@@ -119,23 +125,59 @@ extension LUExpandableTableView: UITableViewDelegate {
         
         return sectionHeader
     }
-    
+
+    /** Tells the delegate that the specified row is now selected.
+ 
+    - Parameters:
+        - tableView: A table-view object informing the delegate about the new row selection.
+        - indexPath: An index path locating the new selected row in `tableView`.
+    */
     public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         expandableTableViewDelegate?.expandableTableView(self, didSelectRowAt: indexPath)
     }
-    
+
+    /** Asks the delegate for the height to use for a row in a specified location
+
+    - Parameters:
+        - tableView: The table-view object requesting this information.
+        - indexPath: An index path that locates a row in `tableView`.
+     
+    - Returns: A nonnegative floating-point value that specifies the height (in points) that row should be.
+    */
     public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return expandableTableViewDelegate?.expandableTableView(self, heightForRowAt: indexPath) ?? UITableViewAutomaticDimension
     }
-    
+
+    /** Asks the delegate for the height to use for the header of a particular section.
+
+    - Parameters:
+        - tableView: The table-view object requesting this information.
+        - section: An index number identifying a section of `tableView`.
+     
+    - Returns: A nonnegative floating-point value that specifies the height (in points) of the header for section.
+    */
     public func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return expandableTableViewDelegate?.expandableTableView(self, heightForHeaderInSection: section) ?? UITableViewAutomaticDimension
     }
-    
+
+    /** Tells the delegate the table view is about to draw a cell for a particular row.
+
+    - Parameters:
+        - tableView: The table-view object informing the delegate of this impending event.
+        - cell: A table-view cell object that tableView is going to use when drawing the row.
+        - indexPath: An index path locating the row in `tableView`.
+    */
     public func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         expandableTableViewDelegate?.expandableTableView(self, willDisplay: cell, forRowAt: indexPath)
     }
-    
+
+    /** Tells the delegate that a header view is about to be displayed for the specified section.
+
+    - Parameters:
+        - tableView: The table-view object informing the delegate of this event.
+        - view: The header view that is about to be displayed
+        - section: An index number identifying a section of `tableView`.
+    */
     public func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
         expandableTableViewDelegate?.expandableTableView(self, willDisplaySectionHeader: view as! LUExpandableTableViewSectionHeader, forSection: section)
     }
@@ -144,14 +186,36 @@ extension LUExpandableTableView: UITableViewDelegate {
 // MARK: - UITableViewDataSource
 
 extension LUExpandableTableView: UITableViewDataSource {
+    /** Tells the data source to return the number of rows in a given section of a table view.
+
+    - Parameters:
+        - tableView: The table-view object requesting this information.
+        - section: An index number identifying a section of `tableView`.
+
+    - Returns: The number of rows in `section`.
+    */
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return expandedSections.contains(section) ? (expandableTableViewDataSource?.expandableTableView(self, numberOfRowsInSection: section) ?? 0) : 0
     }
-    
+
+    /** Asks the data source to return the number of sections in the table view.
+
+    - Parameter tableView: An object representing the table view requesting this information.
+
+    - Returns: The number of sections in `tableView`. The default value is `1`.
+    */
     public func numberOfSections(in tableView: UITableView) -> Int {
         return expandableTableViewDataSource?.numberOfSections(in: self) ?? 0
     }
-    
+
+    /** Asks the data source for a cell to insert in a particular location of the table view.
+
+    - Parameters:
+        - tableView: A table-view object requesting the cell.
+        - indexPath: An index path locating a row in `tableView`.
+
+    - Returns: An object inheriting from `UITableViewCell` that the table view can use for the specified row. An assertion is raised if you return `nil`.
+    */
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return expandableTableViewDataSource?.expandableTableView(self, cellForRowAt: indexPath) ?? UITableViewCell()
     }
@@ -160,6 +224,12 @@ extension LUExpandableTableView: UITableViewDataSource {
 // MARK: - LUExpandableTableViewSectionHeaderDelegate
 
 extension LUExpandableTableView: LUExpandableTableViewSectionHeaderDelegate {
+    /** Tells the delegate that the specified section header should expand or collapse
+
+    - Parameters:
+        - sectionHeader: A section header object informing the delegate of this event
+        - section: An index number identifying the section from a expandable table view that `sectionHeader` is displayed at
+    */
     public func expandableSectionHeader(_ sectionHeader: LUExpandableTableViewSectionHeader, shouldExpandOrCollapseAtSection section: Int) {
         if expandedSections.contains(section) {
             expandedSections.remove(section)
@@ -177,7 +247,13 @@ extension LUExpandableTableView: LUExpandableTableViewSectionHeaderDelegate {
             scrollRectToVisible(sectionHeaderRect, animated: true)
         }
     }
-    
+
+    /** Tells the delegate that the specified section header was selected
+
+    - Parameters:
+        - sectionHeader: A section header object informing the delegate of this event
+        - section: An index number identifying the section from a expandable table view that `sectionHeader` was selected at
+    */
     public func expandableSectionHeader(_ sectionHeader: LUExpandableTableViewSectionHeader, wasSelectedAtSection section: Int) {
         expandableTableViewDelegate?.expandableTableView(self, didSelectSectionHeader: sectionHeader, atSection: section)
     }
